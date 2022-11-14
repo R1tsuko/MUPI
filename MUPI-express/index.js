8dd35b5882c62ce14a321205e06f9d4fb4a3c622
Turkin Danila <nil.ottoman@gmail.com>
2022-11-14 18:20:59 +0400

    cors fix

--- MUPI-express/index.js
+++ MUPI-express/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const serviceRouter = require("./routes/service.route");
 const PORT = process.env.PORT || 8081;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", serviceRouter);
 
